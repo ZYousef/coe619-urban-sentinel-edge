af6b87663ee0c8027beb55bd0fae63f8d4a95736
Joe <62772396+ZYousef@users.noreply.github.com>
2025-03-19 04:02:55 +0300

    minor improvements

--- helpers/run_scripts.py
+++ helpers/run_scripts.py
@@ -1,22 +1,31 @@
 import subprocess
+import os
 
-def run_script(script_name):
-    """ Function to run a Python script """
+def run_script(script_name, *args, is_module=False):
+    """Function to run a Python script or module with optional arguments"""
+    command = ['python3']
+    if is_module:
+        command.extend(['-m', script_name])
+    else:
+        command.append(script_name)
+    command.extend(args)
     try:
-        subprocess.run(['python', script_name], check=True)
+        subprocess.run(command, check=True)
     except subprocess.CalledProcessError:
-        print(f"Failed to execute {script_name}")
+        print(f"Failed to execute {script_name} with args {args}")
 
 def main():
     while True:
         # Run download_loop.py
-        run_script('helpers/download_loop.py')
+        script_path = os.path.join('helpers', 'download_loop.py')
+        run_script(script_path)
         
         # Run fake-location.py
-        run_script('helpers/fake-location.py')
+        script_path = os.path.join('helpers', 'fake-location.py')
+        run_script(script_path)
         
-        # Run main.py
-        run_script('python3 -m accident_detector.main')
+        # Run accident_detector.main as a module with --debug
+        run_script('accident_detector.main', '--debug', is_module=True)
 
 if __name__ == "__main__":
     main()
